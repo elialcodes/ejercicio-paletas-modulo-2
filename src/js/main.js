cb6182cfea1199f3a8427604821fb84f4f4d1a8b
Noelia <abascalcabreran@gmail.com>
2024-03-03 00:18:13 +0100

    favorite container

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const containerPalette = document.querySelector('.js-container'); //contenedor con todas las paletas
+const errorMesagge = document.querySelector('.js-message-error'); //parrafo para posible mensaje de error
 
 //Función para pintar las paletas con 2 bucles anidados: el primero itera para pintar los 5 nombres (h3) de las paletas
-//y el segundo itera para pintar los 5 colores en las 5 paletas.
+//y el segundo itera dentro para pintar los 5 colores en las 5 paletas.
 //Tras haber creado el contenido, del tirón lo pintamos con innerHTML.
+
 function renderPalettes(palettes) {
   let content = '';
   for (const palette of palettes) {
@@ -17,14 +19,32 @@ function renderPalettes(palettes) {
   containerPalette.innerHTML = content;
 }
 
-//Función para pedir datos al servidor y llamar a la función para pintar paletas (renderPalettes)
+//Función para pedir datos al servidor y que los guarda en localStore pasandolos a string:
 
-fetch(
-  'https://beta.adalab.es/ejercicios-de-los-materiales/js-ejercicio-de-paletas/data/palettes.json'
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const palettes = data.palettes; //array de objetos con id, nombre, from y colors
-    renderPalettes(palettes);
-  });
+function getDataApi() {
+  fetch(
+    'https://beta.adalab.es/ejercicios-de-los-materiales/js-ejercicio-de-paletas/data/palettes.json'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      errorMesagge.innerHTML = '';
+      console.log(data);
+      const palettes = data.palettes; //array de objetos con id, nombre, from y colors
+      localStorage.setItem('palettes', JSON.stringify(palettes)); //guardamos los datos recibidos del fetch
+    })
+    .catch(() => {
+      errorMesagge.innerHTML = 'ERROR: Lo sentimos, ha habido un error';
+    });
+}
+
+//constante para guardar los datos de localStorage y los parseamos a JSON:
+const palettesSaved = JSON.parse(localStorage.getItem('palettes'));
+
+//condicional que arranca todo el código: si hay datos en localStorage (palettesSaved),
+//llamamos a la función de pintar paletas con los datos del localStorage como argumento
+// y si no hay datos guardados, llamamos a la función que ejecuta fetch:
+if (palettesSaved !== null) {
+  renderPalettes(palettesSaved);
+} else {
+  getDataApi();
+}
